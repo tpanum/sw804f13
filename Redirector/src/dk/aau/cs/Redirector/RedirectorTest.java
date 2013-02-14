c7b39c253ae12acfb6c360114200e50480cfd81e
Steiniche <call911c@gmail.com>
2013-02-14 11:29:51 +0100

    Fixed configuration management Android

--- Redirector/src/dk/aau/cs/Redirector/RedirectorTest.java
+++ Redirector/src/dk/aau/cs/Redirector/RedirectorTest.java
@@ -1,5 +1,12 @@
 package dk.aau.cs.Redirector;
 
+/**
+ * Created with IntelliJ IDEA.
+ * User: Computer
+ * Date: 14-02-13
+ * Time: 11:27
+ * To change this template use File | Settings | File Templates.
+ */
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
