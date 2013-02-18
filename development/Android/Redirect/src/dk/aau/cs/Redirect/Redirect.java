ee2f2ca29a6705085ba1c65bfd3ab664658297f0
Steiniche <call911c@gmail.com>
2013-02-18 14:54:59 +0100

    Lytzen interview #2

---
+++ development/Android/Redirect/src/dk/aau/cs/Redirect/Redirect.java
@@ -0,0 +1,18 @@
+package dk.aau.cs.Redirect;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class Redirect extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Log.wtf("Rasmus","Hej");
+    }
+}
