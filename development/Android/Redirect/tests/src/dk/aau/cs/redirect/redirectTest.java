8787ce56ba76927d9f4ae786c09c7a937f0210ac
Steiniche <call911c@gmail.com>
2013-02-26 13:48:25 +0100

    Robolectric

--- development/Android/Redirect/tests/src/dk/aau/cs/redirect/redirectTest.java
+++ development/Android/Redirect/tests/src/dk/aau/cs/redirect/redirectTest.java
@@ -1,21 +1,27 @@
-package dk.aau.cs.redirect;
-
-import android.test.ActivityInstrumentationTestCase2;
+package dk.aau.cs.redirect_test;
 
 /**
- * This is a simple framework for a test of an Application.  See
- * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
- * how to write and extend Application tests.
- * <p/>
- * To run this test, you can type:
- * adb shell am instrument -w \
- * -e class dk.aau.cs.redirect.redirectTest \
- * dk.aau.cs.redirect.tests/android.test.InstrumentationTestRunner
+ * Created with IntelliJ IDEA.
+ * User: Computer
+ * Date: 25-02-13
+ * Time: 10:36
+ * To change this template use File | Settings | File Templates.
  */
-public class redirectTest extends ActivityInstrumentationTestCase2<redirect> {
+import dk.aau.cs.redirect.redirect;
+import dk.aau.cs.redirect.R;
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
-    public redirectTest() {
-        super("dk.aau.cs.redirect", redirect.class);
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(RobolectricTestRunner.class)
+public class redirectTest {
+
+    @Test
+    public void shouldHaveProperAppName() throws Exception{
+        String appName = new redirect().getResources().getString(R.string.app_name);
+        assertThat(appName, equalTo("redirect"));
     }
-
 }
