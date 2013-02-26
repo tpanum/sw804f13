5d4e35379fd0b1bf021b8e61de0e2e0efff921bf
Steiniche <call911c@gmail.com>
2013-02-26 13:26:14 +0100

    setting up test project

--- development/Android/Redirect/target/generated-sources/r/dk/aau/cs/redirect/BuildConfig.java
+++ development/Android/Redirect/target/generated-sources/r/dk/aau/cs/redirect/BuildConfig.java
@@ -1,5 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
 package dk.aau.cs.redirect;
 
 public final class BuildConfig {
-  public static final boolean DEBUG = true;
+    public final static boolean DEBUG = true;
 }
