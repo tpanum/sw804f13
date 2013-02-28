8afcacf25ca4319969b8bdf505d6e803522b92f6
Esben <EsbenPM@gmail.com>
2013-02-28 14:23:08 +0100

    new project

---
+++ development/Android/redirector/src/dk/cs/aau/redirector/redirector.java
@@ -0,0 +1,15 @@
+package dk.cs.aau.redirector;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class redirector extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
