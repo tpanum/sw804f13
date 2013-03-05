869a5f3b286a348e816aae7dd5c36c150cf775f5
Bjarke Hesthaven Søndergaard <tixusdk@gmail.com>
2013-03-05 11:52:07 +0100

    Erlang server is now able to respond with valid JSON

--- development/erlang/message.erl
+++ development/erlang/message.erl
@@ -2,12 +2,51 @@
 
 -compile([debug_info, export_all]).
 
-init(Data) ->
-	case mochijson2:decode(Data) of
-		{struct, D} ->
-			D;
-		{error,_} -> {error, <<"Message not of JSON format">>}
+generate_response({message, Message}) ->
+	try
+		{ok, M1} = validate_format(Message),
+		{ok, Command} = determine_command(M1),
+		{ok, Params} = validate_parameters(Command, M1),
+		{ok, {data, D}} = api(Command, Params),
+		form_response(M1, D)
+	catch
+		error:{missing_attribute,Attribute} ->
+			ErrorMessage = list_to_binary(io_lib:format("Missing Attribute: ~s", [Attribute])),
+			form_response(Message, {error, missing_attribute, ErrorMessage});
+		error:{command_not_found,Name} ->
+			ErrorMessage = list_to_binary(io_lib:format("This command is not valid: ~s", [Name])),
+			form_response(Message, {error, command_not_found, ErrorMessage});
+		error:{missing_parameter,Parameter} ->
+			ErrorMessage = list_to_binary(io_lib:format("Missing Parameter: ~s", [Parameter])),
+			form_response(Message, {error, missing_parameter, ErrorMessage})
+		% error:_ ->
+		% 	form_response(Message, {error, error, <<"Unexpected error. Sorry, heavy load.">>})
+	end;
+generate_response(Data) ->
+	try
+		Message = init(Data),
+		generate_response({message, Message})
+	catch
+		error:not_json ->
+			form_response({error, not_json, <<"Received data is not of a valid JSON format">>})
 	end.
+
+init(Data) ->
+	try mochijson2:decode(Data) of
+		{struct, D} ->
+			destruct(D)
+	catch
+		error:_ -> erlang:error(not_json)
+	end.
+
+destruct({struct, L}) ->
+    destruct(L);
+destruct([H | T]) ->
+    [destruct(H) | destruct(T)];
+destruct({K, V}) ->
+    {K, destruct(V)};
+destruct(Term) ->
+    Term.
 
 validate_format(M) ->
 	validate_format(M, [], []).
@@ -24,8 +63,11 @@ validate_format(Message, RequiredAttributes, Result) ->
 	case Attribute of
 		{Target, Value} -> validate_format(Message, RA, [{Target,Value}|Result]);
 
-		false -> error %{error, <<"Missing attribute">>}
+		false -> erlang:error({missing_attribute, Target})
 	end.
+
+determine_command(Message = [{<<"command">>,_}|_]) ->
+	determine_command(lists:keyfind(<<"command">>, 1, Message));
 
 determine_command({<<"command">>, Name}) ->
 	case Name of
@@ -34,48 +76,80 @@ determine_command({<<"command">>, Name}) ->
 		<<"login">> -> {ok, login};
 		<<"fetchContacts">> -> {ok, fetch_contacts};
 		<<"createContact">> -> {ok, create_contact};
-		_ -> error
+		_ -> erlang:error({command_not_found, Name})
 	end;
-determine_command({_,_}) ->
-	error.
+determine_command(_) ->
+	erlang:error(badarg).
 
-validate_parameters(redirect, Message) ->
+validate_parameters(Command, Message = [{<<"command">>,_}|_]) ->
+	% GENERAL CASE
 	{_, Params} = lists:keyfind(<<"params">>, 1, Message),
-	{_, Num} = lists:keyfind(<<"num">>, 1, Params),
+	validate_parameters(Command, Params);
+
+validate_parameters(redirect, Params) ->
+	Num = find_parameter(<<"num">>, Params),
 	{ok, [{num, Num}]};
-validate_parameters(update_status, Message) ->
-	{_, Params} = lists:keyfind(<<"params">>, 1, Message),
-	{_, Status} = lists:keyfind(<<"status">>, 1, Params),
+validate_parameters(update_status, Params) ->
+	Status = find_parameter(<<"status">>, Params),
 	{ok, [{status, Status}]};
-validate_parameters(fetch_contacts, Message) ->
-	{_, _} = lists:keyfind(<<"params">>, 1, Message),
+validate_parameters(fetch_contacts, _) ->
 	{ok, []};
 % validate_parameters(create_contact, Message) ->
 % 	TO BE IMPLEMENTED;
-validate_parameters(login, Message) ->
-	{_, Params} = lists:keyfind(<<"params">>, 1, Message),
-	{_, Username} = lists:keyfind(<<"username">>, 1, Params),
-	{_, Password} = lists:keyfind(<<"password">>, 1, Params),
+validate_parameters(login, Params) ->
+	Username = find_parameter(<<"username">>, Params),
+	Password = find_parameter(<<"password">>, Params),
 	{ok, [{username, Username}, {password, Password}]}.
+
+find_parameter(Param, ListOfParams) ->
+	case lists:keyfind(Param, 1, ListOfParams) of
+		{Param, Value} ->
+			Value;
+		false ->
+			erlang:error({missing_parameter, Param})
+	end.
 
 api(redirect, Params) ->
 	api_redirect(Params);
 api(update_status, Params) ->
-	api_fetch_contacts(Params).
+	api_update_status(Params);
+api(login, Params) ->
+	api_login(Params).
 
 api_redirect(Params) ->
 	% IMPLEMENT CONNECTION TO CALL MANAGER
 	[{num, Num}] = Params,
 	{ok, {data, [{<<"num">>, Num}]}}.
 
-api_fetch_contacts(Params) ->
-	ok.
+api_update_status(Params) ->
+	[{status, Status}] = Params,
+	{ok, {data, [{<<"status">>, Status}]}}.
 
+api_login(Params) ->
+	[{username, Username}, {password, Password}] = Params,
+	% IMPLEMENT stuff about login
+	{ok, {data, [{<<"username">>, Username}, {<<"auth">>, <<"DbsjhfasJJNN23">>}]}}.
+
+form_response({error, _, ErrorMessage}) ->
+	Status = {<<"status">>, [{<<"type">>,<<"error">>}, {<<"message">>,[ErrorMessage]}]},
+
+	Response = [Status],
+	list_to_binary(mochijson2:encode(Response)).
+
+form_response(Message, FullError = {error, _, ErrorMessage}) ->
+	try
+		{_, Timestamp} = lists:keyfind(<<"timestamp">>, 1, Message),
+		TimeTuble = {<<"recv_timestamp">>, Timestamp},
+		Status = {<<"status">>, [{<<"type">>,<<"error">>},{<<"message">>,[ErrorMessage]}]},
+		list_to_binary(mochijson2:encode([Status, TimeTuble]))
+	catch
+		_ -> form_response(FullError)
+	end;
 form_response(Message, Data) ->
 	Command = lists:keyfind(<<"command">>, 1, Message),
 	{_,Timestamp} = lists:keyfind(<<"timestamp">>, 1, Message),
 	TimeTuble = {<<"recv_timestamp">>, Timestamp},
-	Status = {<<"status">>, [{<<"type">>,<<"test">>},{<<"message">>,[]}]},
+	Status = {<<"status">>, [{<<"type">>,<<"ok">>},{<<"message">>,[]}]},
 	DataTuble = {<<"data">>, Data},
 
 	Response = [Command, Status, DataTuble, TimeTuble],
--- development/erlang/message_tests.erl
+++ development/erlang/message_tests.erl
@@ -2,6 +2,18 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+generate_response_test() ->
+    Data = <<"{\"command\":\"redirect\",\"params\":{\"num\":43234444},\"auth\":\"trorlofakod\",\"timestamp\":\"2002-02-02\"}">>,
+    NotJson = <<"\"command\":\"redirect\",\"params\":{\"num\":43234444},\"auth\":\"trorlofakod\",\"timestamp\":\"2002-02-02\"}">>,
+    MissingAttribute = <<"{\"command\":\"redirect\",\"params\":{\"num\":43234444},\"timestamp\":\"2002-02-02\"}">>,
+    CommandNotFound = <<"{\"command\":\"iwanticecream\",\"params\":{\"num\":43234444},\"auth\":\"trorlofakod\",\"timestamp\":\"2002-02-02\"}">>,
+    MissingParameter = <<"{\"command\":\"redirect\",\"params\":{\"test\":43234444},\"auth\":\"trorlofakod\",\"timestamp\":\"2002-02-02\"}">>,
+    ?assertEqual(<<"{\"command\":\"redirect\",\"status\":{\"type\":\"ok\",\"message\":[]},\"data\":{\"num\":43234444},\"recv_timestamp\":\"2002-02-02\"}">>, message:generate_response(Data)),
+    ?assertEqual(<<"{\"status\":{\"type\":\"error\",\"message\":[\"Received data is not of a valid JSON format\"]}}">>, message:generate_response(NotJson)),
+    ?assertEqual(<<"{\"status\":{\"type\":\"error\",\"message\":[\"Missing Attribute: auth\"]},\"recv_timestamp\":\"2002-02-02\"}">>, message:generate_response(MissingAttribute)),
+    ?assertEqual(<<"{\"status\":{\"type\":\"error\",\"message\":[\"This command is not valid: iwanticecream\"]},\"recv_timestamp\":\"2002-02-02\"}">>, message:generate_response(CommandNotFound)),
+    ?assertEqual(<<"{\"status\":{\"type\":\"error\",\"message\":[\"Missing Parameter: num\"]},\"recv_timestamp\":\"2002-02-02\"}">>, message:generate_response(MissingParameter)).
+
 init_test() ->
 	Data = <<"{\"valid\":true}">>,
 	Invalid_data = <<"This is surely not JSON">>,
@@ -15,7 +27,7 @@ validate_format_test() ->
 	Invalid_data = [{<<"thisisnot">>,<<"alist">>}],
 	?assertEqual({ok, [{<<"command">>,<<"true">>}, {<<"params">>,[]}, {<<"auth">>,<<"some_key">>},{<<"timestamp">>, <<"2002-02-02">>}]}, message:validate_format(Data)),
     ?assertEqual({ok, [{<<"command">>,<<"true">>}, {<<"params">>,[]}, {<<"auth">>,<<"some_key">>},{<<"timestamp">>, <<"2002-02-02">>}]}, message:validate_format(UnsortedData)),
-	?assertEqual(error, message:validate_format(Invalid_data)).
+	?assertException(error, {missing_attribute, <<"command">>}, message:validate_format(Invalid_data)).
 
 % perform_command_test() ->
 %     Data = [{<<"command">>, <<"true">>}, {<<"params">>, []}, {<<"auth">>, <<"some_key">>}, {<<"timestamp">>, <<"2002-02-02">>}],
@@ -35,8 +47,8 @@ determine_command_test() ->
     ?assertEqual({ok, login}, message:determine_command(Login)),
     ?assertEqual({ok, fetch_contacts}, message:determine_command(Fetch_contacts)),
     ?assertEqual({ok, create_contact}, message:determine_command(Create_contact)),
-    ?assertEqual(error, message:determine_command(Invalid_command)),
-    ?assertEqual(error, message:determine_command(Invalid_input)).
+    ?assertException(error, {command_not_found, <<"invalid_command">>}, message:determine_command(Invalid_command)),
+    ?assertException(error, badarg, message:determine_command(Invalid_input)).
 
 % determine_parameters_test() ->
 %     Redirect = [{<<"command">>,<<"redirect">>}, {<<"params">>,[{<<"num">>, <<"42135642">>}]}, {<<"auth">>,<<"some_key">>},{<<"timestamp">>, <<"2002-02-02">>}],
@@ -52,23 +64,33 @@ validate_parameters_test() ->
     LoginMessage = [{<<"command">>, <<"login">>}, {<<"params">>, [{<<"trololo">>, <<"dsajjdsaoi">>}, {<<"username">>, <<"bjarkehs">>}, {<<"password">>, <<"thomas er en derp">>}, {<<"num">>, <<"42135642">>}]}, {<<"auth">>, <<"some_key">>}, {<<"timestamp">>, <<"2002-02-02">>}],
     WrongLoginMessage = [{<<"command">>, <<"login">>}, {<<"params">>, [{<<"trololo">>, <<"dsajjdsaoi">>}, {<<"nudsafsam">>, <<"4213fsad25642">>}]}, {<<"auth">>, <<"some_key">>}, {<<"timestamp">>, <<"2002-02-02">>}],
     Fetch_contactsMessage = [{<<"command">>, <<"fetchContacts">>}, {<<"params">>, [{<<"trololo">>, <<"dsajjdsaoi">>}, {<<"username">>, <<"bjarkehs">>}, {<<"password">>, <<"thomas er en derp">>}, {<<"num">>, <<"42135642">>}]}, {<<"auth">>, <<"some_key">>}, {<<"timestamp">>, <<"2002-02-02">>}],
-    WrongFetch_contactsMessage = [{<<"command">>, <<"fetchContacts">>}, {<<"auth">>, <<"some_key">>}, {<<"timestamp">>, <<"2002-02-02">>}],
     ?assertEqual({ok, [{num, <<"42135642">>}]}, message:validate_parameters(redirect, RedirectMessage)),
-    ?assertException(error, _, message:validate_parameters(redirect, WrongRedirectMessage)),
+    ?assertException(error, {missing_parameter, <<"num">>}, message:validate_parameters(redirect, WrongRedirectMessage)),
     ?assertEqual({ok, [{status, <<"42135642">>}]}, message:validate_parameters(update_status, Update_statusMessage)),
-    ?assertException(error, _, message:validate_parameters(update_status, WrongUpdate_statusMessage)),
+    ?assertException(error, {missing_parameter, <<"status">>}, message:validate_parameters(update_status, WrongUpdate_statusMessage)),
     ?assertEqual({ok, [{username, <<"bjarkehs">>}, {password, <<"thomas er en derp">>}]}, message:validate_parameters(login, LoginMessage)),
-    ?assertException(error, _, message:validate_parameters(login, WrongLoginMessage)),
-    ?assertEqual({ok, []}, message:validate_parameters(fetch_contacts, Fetch_contactsMessage)),
-    ?assertException(error, _, message:validate_parameters(fetch_contacts, WrongFetch_contactsMessage)).
+    ?assertException(error, {missing_parameter, <<"username">>}, message:validate_parameters(login, WrongLoginMessage)),
+    ?assertEqual({ok, []}, message:validate_parameters(fetch_contacts, Fetch_contactsMessage)).
 
 api_redirect_test() ->
-    RedirectParams = [{num, <<"213412332">>}],
+    RedirectParams = [{num, 21341233}],
     WrongRedirectParams = [{test, <<"dsadssss">>}],
-    ?assertEqual({ok, {data, [{<<"num">>,<<"213412332">>}]}}, message:api_redirect(RedirectParams)),
+    ?assertEqual({ok, {data, [{<<"num">>,21341233}]}}, message:api_redirect(RedirectParams)),
     ?assertException(error, _, message:api_redirect(WrongRedirectParams)).
+
+api_update_status_test() ->
+    Update_statusParams = [{status, 3}],
+    WrongUpdate_statusParams = [{num, 23124354}],
+    ?assertEqual({ok, {data, [{<<"status">>, 3}]}}, message:api_update_status(Update_statusParams)),
+    ?assertException(error, _, message:api_update_status(WrongUpdate_statusParams)).
+
+api_login_test() ->
+    LoginParams = [{username, <<"bjarkehs">>}, {password, <<"test min hest">>}],
+    WrongLoginParams = [{num, 23124354}, {password, <<"tumpe">>}],
+    ?assertEqual({ok, {data, [{<<"username">>, <<"bjarkehs">>}, {<<"auth">>, <<"DbsjhfasJJNN23">>}]}}, message:api_login(LoginParams)),
+    ?assertException(error, _, message:api_login(WrongLoginParams)).
 
 form_response_test() ->
     Message = [{<<"command">>, <<"redirect">>}, {<<"params">>, [{<<"trololo">>, <<"dsajjdsaoi">>}, {<<"num">>, <<"42135642">>}]}, {<<"auth">>, <<"some_key">>}, {<<"timestamp">>, <<"2002-02-02">>}],
     Data = [{<<"num">>,213412332}],
-    ?assertEqual(<<"{\"command\":\"redirect\",\"status\":{\"type\":\"test\",\"message\":[]},\"data\":{\"num\":213412332},\"recv_timestamp\":\"2002-02-02\"}">>, message:form_response(Message, Data)).
+    ?assertEqual(<<"{\"command\":\"redirect\",\"status\":{\"type\":\"ok\",\"message\":[]},\"data\":{\"num\":213412332},\"recv_timestamp\":\"2002-02-02\"}">>, message:form_response(Message, Data)).
